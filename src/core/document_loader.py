a101de4b86bbcf65b483a97452eb1f852bdb32a9
karthik <karthikgp007@gmail.com>
2026-07-03 15:15:12 +0530

    Updated doc loader after initial tests

--- src/core/document_loader.py
+++ src/core/document_loader.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 from src.core.base import BaseDocumentLoader
 from langchain_docling.loader import DoclingLoader
+from docling.datamodel.pipeline_options import PdfPipelineOptions
+from docling.datamodel.base_models import InputFormat
+from docling.document_converter import DocumentConverter, PdfFormatOption
 from src.config import RagConfig
 from src.models.schemas import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
@@ -21,7 +24,14 @@ class DoclingDocumentLoader(BaseDocumentLoader):
         if not path.is_absolute():
             path = Path(__file__).resolve().parent.parent.parent / "data" / self.file_path
         
-        loader = DoclingLoader(file_path=path)
+        pipeline_options = PdfPipelineOptions()
+        pipeline_options.do_ocr = False
+        converter = DocumentConverter(
+            format_options={
+                InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)
+            }
+        )
+        loader = DoclingLoader(file_path=path, converter=converter)
         docs = loader.load()
         splitter = RecursiveCharacterTextSplitter(
             chunk_size=self.config.chunking.chunk_size,
